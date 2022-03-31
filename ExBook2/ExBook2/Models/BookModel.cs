cd18f7a9515a6c8ff1a251a31f65d2d0923a1205
Maram <marambasem18@gmail.com>
2022-03-31 18:21:11 +0300

    sellpage

---
+++ ExBook2/ExBook2/Models/BookModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExBook2.Models
+{
+    internal class BookModel
+    {
+        public string BookName { get; set; }
+        public string AuthorName { get; set; }
+        public int EditionNumber { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public int salary { get; set; }
+        public string CollegeName { get; set; }
+        public string Description { get; set; } //Changeable
+        // image ?
+    }
+}
---
+++ ExBook2/ExBook2/ViewModels/AddBookSellViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExBook2.ViewModels
+{
+    internal class AddBookSellViewModel
+    {
+        public string BookName { get; set; }
+    }
+}
